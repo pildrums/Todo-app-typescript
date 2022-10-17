5cd30e6f019dfac01c9d3429dfc7b94a32ba7f0a
Pildrums <there7788@gmail.com>
2022-10-17 15:34:55 +0900

    Added todoList function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 import TodoList from "./components/TodoList";
 
-const App = () => {
+function App() {
   return (
     <>
       <GlobalStyle />
       <TodoList />
     </>
   );
-};
+}
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');
