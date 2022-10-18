69b1806d89fb80f1abe99469e5a7b612b662fe01
Pildrums <there7788@gmail.com>
2022-10-18 19:03:01 +0900

    Divided todo, doing, done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,36 @@
 import { useRecoilValue } from "recoil";
-import { todoState } from "./atoms";
+import { todoSelector } from "./atoms";
 import CreateTodo from "./CreateTodo";
 import Todo from "./Todo";
 
 function TodoList() {
-  const todos = useRecoilValue(todoState);
-  console.log(todos);
+  const [todo, doing, done] = useRecoilValue(todoSelector);
   return (
     <div>
-      <h1>Todo</h1>
+      <h1>Todos</h1>
       <hr />
       <CreateTodo />
+      <h2>Todo</h2>
       <ul>
-        {todos.map((todo) => (
-          <Todo {...todo} key={todo.id} />
+        {todo.map((item) => (
+          <Todo {...item} key={item.id} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((item) => (
+          <Todo {...item} key={item.id} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((item) => (
+          <Todo {...item} key={item.id} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,4 +1,9 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+/**
+ * selector는 atom의 output을 변형시키는 도구
+ * 즉 기존에 만들어 놓은 atom을 변화를 줄 수 있음
+ */
 
 export interface ITodo {
   text: string;
@@ -9,4 +14,16 @@ export interface ITodo {
 export const todoState = atom<ITodo[]>({
   key: "todo",
   default: [],
+});
+
+export const todoSelector = selector({
+  key: "todoSelector",
+  get: ({ get }) => {
+    const todos = get(todoState);
+    return [
+      todos.filter((todo) => todo.category === "TODO"),
+      todos.filter((todo) => todo.category === "DOING"),
+      todos.filter((todo) => todo.category === "DONE"),
+    ];
+  },
 });
