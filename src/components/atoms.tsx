5a26b7643cc128a149d9267ce7a2bd0a7511a56f
Pildrums <there7788@gmail.com>
2022-10-18 22:30:49 +0900

    Modified todoSelector function

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -26,11 +26,6 @@ export const todoSelector = selector({
   get: ({ get }) => {
     const todos = get(todoState);
     const category = get(categoryState);
-    if (category === "TODO")
-      return todos.filter((todo) => todo.category === "TODO");
-    if (category === "DOING")
-      return todos.filter((todo) => todo.category === "DOING");
-    if (category === "DONE")
-      return todos.filter((todo) => todo.category === "DONE");
+    return todos.filter((todo) => todo.category === category);
   },
 });
